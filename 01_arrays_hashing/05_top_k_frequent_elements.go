3e9780de51be4afe6a56c1491a1e0e9e24534765
Sergey Yurich <yurichsergey@gmail.com>
2025-12-21 01:01:25 +0400

    Add `topKFrequent` implementation with test cases

---
+++ 01_arrays_hashing/05_top_k_frequent_elements.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+)
+
+/*
+Top K Frequent Elements
+
+Medium
+
+Given an integer array nums and an integer k, return the k most frequent elements within the array.
+
+The test cases are generated such that the answer is always unique.
+
+You may return the output in any order.
+
+Example 1:
+
+Input: nums = [1,2,2,3,3,3], k = 2
+
+Output: [2,3]
+Example 2:
+
+Input: nums = [7,7], k = 1
+
+Output: [7]
+Constraints:
+
+1 <= nums.length <= 10^4.
+-1000 <= nums[i] <= 1000
+1 <= k <= number of distinct elements in nums.
+
+
+Recommended Time & Space Complexity
+You should aim for a solution with O(n) time and O(n) space, where n is the size of the input array.
+
+
+Hint 1
+A naive solution would be to count the frequency of each number and then sort the array based on each element’s
+frequency. After that, we would select the top k frequent elements. This would be an O(nlogn) solution.
+Though this solution is acceptable, can you think of a better way?
+
+
+Hint 2
+Can you think of an algorithm which involves grouping numbers based on their frequency?
+
+
+Hint 3
+Use the bucket sort algorithm to create n buckets, grouping numbers based on their frequencies from 1 to n.
+Then, pick the top k numbers from the buckets, starting from n down to 1.
+
+https://neetcode.io/problems/top-k-elements-in-list/question?list=neetcode150
+
+*/
+
+func main() {
+	testCases := []struct {
+		nums []int
+		k    int
+		res  []int
+	}{
+		{nums: []int{1, 2, 2, 3, 3, 3}, k: 2, res: []int{2, 3}},
+		{nums: []int{7, 7}, k: 1, res: []int{7}},
+	}
+
+	for _, tc := range testCases {
+		result := topKFrequent(tc.nums, tc.k)
+		sort.Ints(result)
+		sort.Ints(tc.res)
+		fmt.Printf(
+			"nums: %v, k: %d, expected: %v, got: %v, match: %v\n",
+			tc.nums,
+			tc.k,
+			tc.res,
+			result,
+			reflect.DeepEqual(result, tc.res),
+		)
+	}
+}
+
+func topKFrequent(nums []int, k int) []int {
+
+	freq := map[int]int{}
+	for _, i := range nums {
+		freq[i] += 1
+	}
+
+	keys := make([]int, 0, len(freq))
+	for i := range freq {
+		keys = append(keys, i)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return freq[keys[i]] > freq[keys[j]]
+	})
+
+	//fmt.Println("print freq")
+	//for i := range(freq) {
+	//	fmt.Println("%i - %i", i, freq[i])
+	//}
+	//fmt.Println("print keys")
+	//for i := range(keys) {
+	//	fmt.Println("%i", keys[i])
+	//}
+
+	res := make([]int, 0, k)
+	for i := 0; i < k; i++ {
+		res = append(res, keys[i])
+	}
+	return res
+}
