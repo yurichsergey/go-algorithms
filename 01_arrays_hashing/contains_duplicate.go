fe3ffe6c414a23ec03dde2cd6974a4332dc8a574
Sergey Yurich <yurichsergey@gmail.com>
2025-12-21 00:03:28 +0400

    Add `contains_duplicate` implementation and initial `.gitignore`

---
+++ 01_arrays_hashing/contains_duplicate.go
@@ -0,0 +1,58 @@
+// package _1_arrays_hashing
+package main
+
+import "fmt"
+
+/*
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+
+https://neetcode.io/problems/duplicate-integer/question
+
+Time complexity:
+O(n)
+O(n)
+Space complexity:
+O(n)
+O(n)
+n is the number of elements in the nums slice.
+*/
+
+func main() {
+	testCases := []struct {
+		data []int
+		res  bool
+	}{
+		{data: []int{1, 2, 3, 3}, res: true},
+		{data: []int{1, 2, 3, 4}, res: false},
+	}
+
+	for _, tc := range testCases {
+		result := hasDuplicate(tc.data)
+		fmt.Printf("data: %v, expected: %v, got: %v\n", tc.data, tc.res, result)
+	}
+}
+
+func hasDuplicate(nums []int) bool {
+	set := map[int]struct{}{}
+	for _, v := range nums {
+		_, ok := set[v]
+		if ok {
+			return true
+		} else {
+			set[v] = struct{}{}
+		}
+	}
+	return false
+}
