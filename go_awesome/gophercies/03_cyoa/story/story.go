02a6d50be01b0ada61ebfcf6fb73cd55930cb0fb
Sergey <yurichsergey@gmail.com>
2026-05-25 23:15:06 +0400

    feat(gophercies): add CYOA story engine with JSON story parsing

---
+++ go_awesome/gophercies/03_cyoa/story/story.go
@@ -0,0 +1,39 @@
+package story
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type Story map[string]Arc
+
+type Option struct {
+	Text string `json:"text,omitempty"`
+	Arc  string `json:"arc,omitempty"`
+}
+
+type Arc struct {
+	Title   string   `json:"title,omitempty"`
+	Story   []string `json:"story,omitempty"`
+	Options []Option `json:"options,omitempty"`
+}
+
+func FromJSON(r io.Reader) (Story, error) {
+	var s Story
+	if err := json.NewDecoder(r).Decode(&s); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+func FromJSONUnmarshal(r io.Reader) (Story, error) {
+	var s Story
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(data, &s); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
