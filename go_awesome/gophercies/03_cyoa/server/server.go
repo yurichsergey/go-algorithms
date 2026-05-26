d7aa6b1b7712397f7a54ab38e71d2ae286a8b896
Sergey <yurichsergey@gmail.com>
2026-05-27 00:46:14 +0400

    refactor(gophercies): replace manual HTML generation with template rendering in CYOA server

--- go_awesome/gophercies/03_cyoa/server/server.go
+++ go_awesome/gophercies/03_cyoa/server/server.go
@@ -1,11 +1,35 @@
 package server
 
 import (
+	"html/template"
 	"net/http"
 	"strings"
 
 	"github.com/yurichsergey/go-algorithms/go_awesome/gophercies/03_cyoa/story"
 )
+
+const tpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>{{.Title}}</title>
+</head>
+<body>
+  <h1>{{.Title}}</h1>
+  {{range .Story}}
+	<p>{{.}}</p>
+  {{end}}
+  {{if .Options}}
+    <ul>
+    {{range .Options}}
+      <li><a href="/{{.Arc}}">{{.Text}}</a></li>
+    {{end}}
+    </ul>
+  {{end}}
+</body>
+</html>
+`
 
 func NewRequestHandler(s story.Story) http.Handler {
 	return RequestHandler{Story: s}
@@ -26,20 +50,8 @@ func (h RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	storyHTML := `<h1>` + storyPart.Title + `</h1>`
-	for _, paragraph := range storyPart.Story {
-		storyHTML += `<p>` + paragraph + `</p>`
-	}
-
-	if len(storyPart.Options) > 0 {
-		storyHTML += `<p>Continue<ul>`
-		for _, choice := range storyPart.Options {
-			storyHTML += `<li><a href="` + choice.Arc + `">` + choice.Text + `</a></li>`
-		}
-		storyHTML += `</ul></p>`
-	}
-
-	if _, err := w.Write([]byte(storyHTML)); err != nil {
+	tmpl := template.Must(template.New("story").Parse(tpl))
+	if err := tmpl.Execute(w, storyPart); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
