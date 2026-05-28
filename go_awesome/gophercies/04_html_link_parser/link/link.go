b47828c86bbf5ad11c272d0b9f478c7f7930546f
Sergey <yurichsergey@gmail.com>
2026-05-28 23:30:12 +0400

    feat(gophercies): enhance link parser with text extraction and add CLI for parsing HTML files

--- go_awesome/gophercies/04_html_link_parser/link/link.go
+++ go_awesome/gophercies/04_html_link_parser/link/link.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -33,18 +34,34 @@ func walk(n *html.Node) []Link {
 			href = ""
 		}
 
-		text := ""
-		// @todo when some elements there
-		if n.FirstChild == n.LastChild && n.FirstChild.Type == html.TextNode {
-			text = n.FirstChild.Data
+		text := getText(n)
+		if len(text) == 0 {
+			text = href
 		}
 
-		if len(href) > 0 && len(text) > 0 {
+		if len(href) > 0 {
 			links = append(links, Link{Href: href, Text: text})
 		}
+		return links
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		links = append(links, walk(c)...)
 	}
 	return links
+}
+
+func getText(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return strings.Join(strings.Fields(n.Data), " ")
+	}
+	if n.Type == html.CommentNode {
+		return ""
+	}
+	var text []string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if t := getText(c); len(t) > 0 {
+			text = append(text, t)
+		}
+	}
+	return strings.Join(strings.Fields(strings.Join(text, " ")), " ")
 }
--- go_awesome/gophercies/04_html_link_parser/main.go
+++ go_awesome/gophercies/04_html_link_parser/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/yurichsergey/go-algorithms/go_awesome/gophercies/04_html_link_parser/link"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: go run main.go <html_file>")
+	}
+	filename := os.Args[1]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	links, err := link.Parse(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, url := range links {
+		fmt.Printf("url = \"%s\", %s\n", url.Href, url.Text)
+	}
+}
