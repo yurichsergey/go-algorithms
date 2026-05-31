3db601b4b585ff6a0e5768b868fe18301da52e25
Sergey <yurichsergey@gmail.com>
2026-05-31 22:03:18 +0400

    feat(gophercies): extract crawl logic to a dedicated package, improve modularity

--- go_awesome/gophercies/05_sitemap/main.go
+++ go_awesome/gophercies/05_sitemap/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
+	"05_sitemap/crawl"
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
-
-	"link_parser/link"
 )
 
 func main() {
@@ -22,84 +18,7 @@ func main() {
 	}
 	fmt.Printf("startURL = \"%s\"\n", startURL)
 
-	crawl(startURL)
+	crawl.Crawl(startURL)
 
 	//fmt.
-}
-
-func crawl(startURL string) []string {
-	parsedStartURL, err := url.Parse(startURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	queue := make([]string, 0)
-	queue = append(queue, startURL)
-
-	visited := make(map[string]struct{})
-	for len(queue) > 0 {
-		currentHref := queue[0]
-		queue = queue[1:]
-		if _, ok := visited[currentHref]; ok {
-			continue
-		}
-		queue = downloadAndAnalyzeURL(currentHref, parsedStartURL, queue)
-		visited[currentHref] = struct{}{}
-	}
-
-	fmt.Printf("Visited URLs = %d\n", len(visited))
-	for visitedURL := range visited {
-		fmt.Println(visitedURL)
-	}
-
-	var visitedHrefs []string
-	for visitedURL := range visited {
-		visitedHrefs = append(visitedHrefs, visitedURL)
-	}
-	return visitedHrefs
-}
-
-func downloadAndAnalyzeURL(currentHref string, parsedStartURL *url.URL, queue []string) []string {
-	currentLinks, errExtract := extractLinksFromUrl(currentHref)
-	if errExtract != nil {
-		log.Printf("Error extracting links from URL \"%s\": %s", currentHref, errExtract)
-		return queue
-	}
-	for _, currentLink := range currentLinks {
-		fmt.Printf("Processing URL \"%s\"\n", currentLink.Href)
-		parsedCurrentURL, errParse := url.Parse(currentLink.Href)
-		if errParse != nil {
-			log.Printf("Error parsing URL \"%s\": %s", currentLink.Href, errParse)
-		}
-		if parsedCurrentURL.Host != parsedStartURL.Host && parsedCurrentURL.Host != "" {
-			continue
-		}
-		URLToAdd := normalizeURL(parsedStartURL, parsedCurrentURL)
-		//URLToAdd := parsedStartURL.ResolveReference(parsedCurrentURL).String()
-		fmt.Printf("Adding URL \"%s\" to the queue\n", URLToAdd)
-		queue = append(queue, URLToAdd)
-	}
-	return queue
-}
-
-func normalizeURL(baseURL *url.URL, customURL *url.URL) string {
-	return baseURL.ResolveReference(customURL).String()
-}
-
-func extractLinksFromUrl(url string) ([]link.Link, error) {
-	log.Printf("Fetching links from URL \"%s\"", url)
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(resp.Body)
-
-	links, err := link.Parse(resp.Body)
-	log.Printf("Parsed %d links", len(links))
-	return links, err
 }
