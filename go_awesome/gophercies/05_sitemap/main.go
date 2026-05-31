e798661e35ead78cd42eda310727ee1d0b92c390
Sergey <yurichsergey@gmail.com>
2026-05-31 22:22:41 +0400

    feat(gophercies): add XML sitemap generation functionality with encoding and namespace support

--- go_awesome/gophercies/05_sitemap/main.go
+++ go_awesome/gophercies/05_sitemap/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"05_sitemap/crawl"
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"log"
 )
+
+type XMLUrl struct {
+	Loc string `xml:"loc"`
+}
+
+type XMLUrlSet struct {
+	XMLName xml.Name `xml:"urlset"`
+	XMLNS   string   `xml:"xmlns,attr"`
+	URLs    []XMLUrl `xml:"url"`
+}
+
+func (x *XMLUrlSet) toXML(links []string) (string, error) {
+	for _, link := range links {
+		x.URLs = append(x.URLs, XMLUrl{Loc: link})
+	}
+
+	output, err := xml.MarshalIndent(x, "", " ")
+	return xml.Header + string(output), err
+}
 
 func main() {
 
@@ -18,7 +38,13 @@ func main() {
 	}
 	fmt.Printf("startURL = \"%s\"\n", startURL)
 
-	crawl.Crawl(startURL)
+	links := crawl.Crawl(startURL)
 
-	//fmt.
+	xmlUrlSet := XMLUrlSet{XMLNS: "http://www.sitemaps.org/schemas/sitemap/0.9"}
+	xmlOutput, err := xmlUrlSet.toXML(links)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n\n=====\n", xmlOutput)
 }
